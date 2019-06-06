20807899665a60769b2acdd1ae42f6ca6c79ceb9
Jalil KETTANI <jkettani@e2r2p3.42.fr>
2019-06-06 18:04:51 +0200

    Add check if file exist in checker script

--- check_invalid_map.sh
+++ check_invalid_map.sh
@@ -13,12 +13,17 @@ TEST_TMP=test_temp
 
 print_ok()
 {
-	printf "${GREEN}%s${RESET}" $1
+	printf "${GREEN}%s${RESET}" "$1"
 }
 
 print_error()
 {
-	printf "${RED}%s${RESET}" $1
+	printf "${RED}%s${RESET}" "$1"
+}
+
+print_warning()
+{
+	printf "${YELLOW}%s${RESET}" "$1"
 }
 
 run_test()
@@ -26,15 +31,19 @@ run_test()
 	local name=`echo $@ | cut -d';' -f1`
 	local map=`echo $@ | cut -d';' -f2`
 
-	printf "%-40s" "$name"
-		${PROJECT_PATH}/lem-in < "${MAP_PATH}/${map}" > /dev/null 2> ${TEST_TMP}
-	local output=`cat -e ${TEST_TMP}`
-	if [ "${output:0:5}" = "ERROR" ]; then
-		print_ok "Good!"
+	printf "%-50s" "$name"
+	if [ -f "${MAP_PATH}/${map}" ];then
+		${PROJECT_PATH}/lem-in -e < "${MAP_PATH}/${map}" > /dev/null 2> ${TEST_TMP}
+		local output=`cat -e ${TEST_TMP}`
+		if [ "${output:0:5}" = "ERROR" ]; then
+			print_ok "Good!"
+		else
+			print_error "Booo!"
+		fi
+		printf "%5s%-60s" "" "`cat ${TEST_TMP}`"
 	else
-		print_error "Booo!"
+		print_warning "File not found"
 	fi
-	printf "%5s%-60s" "" "`cat ${TEST_TMP}`"
 	printf "\n"
 }
 
@@ -42,4 +51,4 @@ while read line
 do
 	run_test ${line}
 done < ${INPUT_DATA}
-rm $TEST_TMP
+#rm $TEST_TMP

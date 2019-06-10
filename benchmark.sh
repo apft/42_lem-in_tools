99bb2961bf745445c865882fca00f7346d1314dc
Alexandre PION <apion@student.42.fr>
2019-06-10 14:49:18 +0200

    Use args to define a lem-in binary file

--- benchmark.sh
+++ benchmark.sh
@@ -1,6 +1,7 @@
-FILE="$@"
+FILE="$1"
+[ "$2" ] && EXEC="$2" || EXEC="./lem-in"
 
-PATHS=`./lem-in < $FILE | grep ^L | head -n 1`
+PATHS=`$EXEC < $FILE | grep ^L | head -n 1`
 NB_PATHS=`echo "$PATHS" | wc -w | bc`
 
 echo "Paths:"
@@ -9,8 +10,8 @@ echo "\nNb of paths: $NB_PATHS"
 i=1
 while [ $i -le $NB_PATHS ];
 do
-	path_length=`./lem-in < $FILE | grep L$i- | wc -l | bc`
-	path_name=`echo $PATHS | tr ' ' '\n' | grep L$i | cut -d '-' -f2`
+	path_length=`$EXEC < $FILE | grep "L$i-" | wc -l | bc`
+	path_name=`echo $PATHS | tr ' ' '\n' | grep "L$i-" | cut -d '-' -f2`
 	printf "%d: (%d, %s)" $i $path_length $path_name
 	if [ $i -lt $NB_PATHS ];then
 		printf " | "

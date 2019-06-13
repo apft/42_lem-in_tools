a3a313f46c9a3aff1f0c8fa3dd5e0a97be6babb9
Jalil KETTANI <jkettani@e2r2p3.42.fr>
2019-06-13 12:18:05 +0200

    Store output in a temporary file in benchmark.sh

--- benchmark.sh
+++ benchmark.sh
@@ -1,7 +1,10 @@
 FILE="$1"
 [ "$2" ] && EXEC="$2" || EXEC="./lem-in"
 
-PATHS=`$EXEC < $FILE | grep ^L | head -n 1`
+TMP_OUT=tmp_out
+
+$EXEC < $FILE > $TMP_OUT
+PATHS=`cat $TMP_OUT | grep ^L | head -n 1`
 NB_PATHS=`echo "$PATHS" | wc -w | bc`
 
 echo "Paths:"
@@ -10,7 +13,7 @@ echo "\nNb of paths: $NB_PATHS"
 i=1
 while [ $i -le $NB_PATHS ];
 do
-	path_length=`$EXEC < $FILE | grep "L$i-" | wc -l | bc`
+	path_length=`cat $TMP_OUT | grep "L$i-" | wc -l | bc`
 	path_name=`echo $PATHS | tr ' ' '\n' | grep "L$i-" | cut -d '-' -f2`
 	printf "%d: (%d, %s)" $i $path_length $path_name
 	if [ $i -lt $NB_PATHS ];then
@@ -20,3 +23,4 @@ do
 done
 printf "\n"
 
+rm -f $TMP_OUT

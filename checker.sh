87dad180ffc87c04e90abb6f4d3fa7e3c536828d
Alexandre PION <apion@student.42.fr>
2019-06-17 16:35:32 +0200

    Store 'start' and 'end' room, use color in dislayed messages

--- checker.sh
+++ checker.sh
@@ -7,6 +7,14 @@
 # verifier que le chemin est valide
 # lecture ligne a ligne de la sortie du prgm ./lem-in
 
+# Colors
+RED='\x1b[1;31m'
+GREEN='\x1b[1;32m'
+YELLOW='\x1b[1;33m'
+BLUE='\x1b[1;34m'
+MAGENTA='\x1b[1;35m'
+NC='\x1b[0m'
+
 EXEC="$1"
 MAP="$2"
 
@@ -16,8 +24,42 @@ $EXEC < $MAP > $OUTPUT
 NB_ANTS=`head -n 1 $MAP`
 NB_PATH=`grep "^L" $OUTPUT | head -n 1 | grep -o "L" | wc -l | bc`
 
-echo "ants: ${NB_ANTS}"
-echo "path: ${NB_PATH}"
+function rm_tmp_files()
+{
+	for file in $@
+	do
+		[ -f "$file" ] && rm $file
+	done
+}
+
+# Print error message
+print_error(){
+	printf "${RED}%s${NC}\n" "$1"
+}
+
+# Print message ok
+print_ok(){
+	printf "${GREEN}%s${NC}\n" "$1"
+}
+
+# Print warning message
+print_warn(){
+	printf "${YELLOW}%s${NC}\n" "$1"
+}
+
+
+function extract_command()
+{
+	local room=$1
+
+	grep -A 1 "##$room" $MAP | tail -n 1 | cut -d ' ' -f 1
+}
+
+ROOM_START=`extract_command "start"`
+ROOM_END=`extract_command "end"`
+echo "start: ${ROOM_START}"
+echo "end: ${ROOM_END}"
+
 
 function print_paths()
 {
@@ -34,6 +76,10 @@ function length()
 	local	index="$1"
 	grep "L${index}-" $OUTPUT | wc -l | bc
 }
+
+echo "ants: ${NB_ANTS}"
+echo "path: ${NB_PATH}"
+
 
 print_paths
 
@@ -54,4 +100,4 @@ do
 	fi
 done
 
-[ -f "$OUTPUT" ] && rm "$OUTPUT"
+rm_tmp_files $OUTPUT
